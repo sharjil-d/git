b7438253dda1763689ebe451ea9fd83a0d110a40
sharj <sharj@LAPTOP-TVU82KVB>
2022-07-24 15:52:17 +0530

    this is my first commit

---
+++ src/git/test1.java
@@ -0,0 +1,14 @@
+package git;
+
+public class test1 {
+	public static void main(String[] args)
+	{
+		String hungry="yes";
+		
+		if(hungry=="yes")
+			System.out.println("eatt");
+		else
+			System.out.println("dont eatt");
+	}
+
+}
